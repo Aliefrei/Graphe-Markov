3c373e1c934237497e22c877e0e489a3a81bc8b0
charaf <charafelmansari1@gmail.com>
2025-11-10 18:42:05 +0100

    Etape 1 part 2

--- part2.c
+++ part2.c
@@ -1,5 +1,18 @@
 //
 // Created by alilo on 10/11/2025.
 //
+#include "part1.h"
+#include "part2.h"
 
-
+tab_all_vertex load_tab(t_list_adj graph){
+    tab_all_vertex mytab_vertex;
+    mytab_vertex.taille = graph.taille;
+    mytab_vertex.vertex = malloc(sizeof(t_tarjan_vertex) * mytab_vertex.taille);
+    for(int i = 0; i < mytab_vertex.taille; i++){
+      mytab_vertex.vertex[i].id=i+1;
+      mytab_vertex.vertex[i].num_tmp=-1;
+      mytab_vertex.vertex[i].num_vertex=-1;
+      mytab_vertex.vertex[i].booleen=0;
+    }
+    return mytab_vertex;
+   } 
--- part2.h
+++ part2.h
@@ -4,6 +4,7 @@
 
 #ifndef PART2_H
 #define PART2_H
+#include "part1.h"
 
 typedef struct s_tarjan_vertex{
   int id;
@@ -13,6 +14,7 @@ typedef struct s_tarjan_vertex{
   } t_tarjan_vertex;
 
 typedef struct {
+  int taille;
   t_tarjan_vertex* vertex;
 }tab_all_vertex;
 
@@ -21,8 +23,21 @@ typedef struct s_class{
   t_tarjan_vertex *nb_vertex;
 }t_class;
 
+typedef struct s_list_class{
+  int taille;
+  t_class** tab;
+}t_list_class;
 
 //FONCTIONS
+
+tab_all_vertex tab_all_vertex_create(t_list_adj list);
+
+typedef struct s_pile{
+  int taille;
+  t_tarjan_vertex** tab;
+}t_pile;
+
+
 
 
 
