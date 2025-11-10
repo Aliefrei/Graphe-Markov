aace1e0f9163c162b8da8f137466197103e30d43
Ali <alilouelhajjam@gmail.com>
2025-11-10 17:44:20 +0100

    PUSH etape 1 Part 2

---
+++ part2.c
@@ -0,0 +1,5 @@
+//
+// Created by alilo on 10/11/2025.
+//
+
+
---
+++ part2.h
@@ -0,0 +1,41 @@
+//
+// Created by alilo on 10/11/2025.
+//
+
+#ifndef PART2_H
+#define PART2_H
+
+typedef struct s_tarjan_vertex{
+  int id;
+  int num_tmp;
+  int num_vertex;
+  int booleen;
+  } t_tarjan_vertex;
+
+typedef struct {
+  t_tarjan_vertex *vertex;
+}tab_all_vertex;
+
+typedef struct s_class{
+  char *nom_class;
+  t_tarjan_vertex *vertex;
+}t_class;
+
+
+//FONCTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //PART2_H
